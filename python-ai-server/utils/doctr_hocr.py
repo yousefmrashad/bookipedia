7b4f8af928ea7791ddb383963a99a36fec4499a1
yousefmrashad <yousefrashad4444@yahoo.com>
2024-02-09 01:47:05 +0200

    Add deskew functionality to OCR scripts. Used db_mobilenet_v3_large for increased performance; db_resnet50 is redundant.

--- python-ai-server/utils/doctr_hocr.py
+++ python-ai-server/utils/doctr_hocr.py
@@ -1,30 +1,47 @@
 # Imports
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
-from PIL import Image
+from PIL.Image import fromarray
 from hocr import HocrTransform
 import sys
 import time
 import pikepdf
+from deskew import determine_skew
+import numpy as np
+from skimage.transform import rotate
+from skimage.color import rgb2gray
+
+def deskew(docs):
+    """
+    Deskew the images using the deskew library.
+    """
+    docs_desk = []
+    for doc in docs:
+        gray = rgb2gray(doc)
+        angle = determine_skew(gray)
+        rotated = rotate(doc, angle, resize= True) * 255
+        docs_desk.append(rotated.astype(np.uint8))
+    return docs_desk
 
 def main():
     """
     Entry point of the script.
     Extracts text from images or PDFs using OCR and saves the result as an hOCR PDF file.
 
-    Usage: python doctr_hocr.py [file path] [file type]: {img, pdf}
+    Usage: python doctr_hocr.py [file path] [file type]: {img, pdf} [deskew(optional)]: {deskew, None}
+
     """
 
     start_time = time.time()
 
     # Check if two arguments are provided
 
-    if len(sys.argv) != 3:
-        print("Usage: python doctr_hocr.py [file path] [file type]: {img, pdf}")
+    if len(sys.argv) < 3:
+        print("Usage: python doctr_hocr.py [file path] [file type]: {img, pdf} [deskew(optional)]: {deskew, None}")
         return
     
     if sys.argv[1] == "help" or sys.argv[1] == "-h":
-        print("Usage: python doctr_hocr.py [file path] [file type]: {img, pdf}")
+        print("Usage: python doctr_hocr.py [file path] [file type]: {img, pdf} [deskew(optional)]: {deskew, None}")
         return
     
 
@@ -35,14 +52,25 @@ def main():
 
     type = sys.argv[2]
 
+    if len(sys.argv) == 4: 
+        desk = sys.argv[3]
+        if desk == "deskew":
+            desk = True
+    else:
+        desk = False
+    
     if type == "img":
         docs = DocumentFile.from_images(doc)
     elif type == "pdf":
         docs = DocumentFile.from_pdf(doc, scale = 4.1667)
+    
+    if desk:
+        # Deskew the images
+        docs = deskew(docs)
 
-    model = ocr_predictor(det_arch='db_resnet50_rotation',
+    model = ocr_predictor(det_arch='db_mobilenet_v3_large',
                           reco_arch='crnn_mobilenet_v3_large',
-                          assume_straight_pages= False,
+                          assume_straight_pages= True,
                           pretrained=True,
                           export_as_straight_boxes= True).cuda()
 
@@ -59,7 +87,7 @@ def main():
             dpi=300
         )
 
-        pdf = hocr.to_pdf(image=Image.fromarray(img))
+        pdf = hocr.to_pdf(image=fromarray(img))
         
         pdf_output.pages.extend(pdf.pages)
 
--- python-ai-server/utils/tess_hocr.py
+++ python-ai-server/utils/tess_hocr.py
@@ -1,31 +1,47 @@
 # Imports
 from PyPDF2 import PdfMerger, PdfReader
-from PIL import Image
 import sys
 import time
 import pytesseract
 from pdf2image import convert_from_path
 import os
 import io
+from deskew import determine_skew
+import numpy as np
+from skimage.io import imread
+from skimage.transform import rotate
+from skimage.color import rgb2gray
+
+def deskew(img):
+    """
+    Deskew the images using the deskew library.
+    """
+    img = img[:, :, :3]
+    gray = rgb2gray(img)
+    angle = determine_skew(gray)
+    rotated = rotate(img, angle, resize= True) * 255
+    img_desk = rotated.astype(np.uint8)
+    return img_desk
 
 def main():
     """
     Entry point of the program.
     
-    This function takes two command-line arguments: [file path] and [file type].
+    This function takes two command-line arguments: [file path], [file type], [deskew].
     It processes the specified document based on the provided file type and generates an hOCR PDF file.
+    
     """
     
     start_time = time.time()
 
     # Check if two arguments are provided
 
-    if len(sys.argv) != 3:
-        print("Usage: python tess_hocr.py [file path] [file type]: {img, im_dir, pdf}")
+    if len(sys.argv) < 3:
+        print("Usage: python tess_hocr.py [file path] [file type]: {img, im_dir, pdf} [deskew(optional)]: {deskew, None}")
         return
     
     if sys.argv[1] == "help" or sys.argv[1] == "-h":
-        print("Usage: python tess_hocr.py [file path] [file type]: {img, im_dir, pdf}")
+        print("Usage: python tess_hocr.py [file path] [file type]: {img, im_dir, pdf} [deskew(optional)]: {deskew, None}")
         return
     
 
@@ -36,16 +52,23 @@ def main():
 
     type = sys.argv[2]
 
+    if len(sys.argv) == 4: 
+        desk = sys.argv[3]
+        if desk == "deskew":
+            desk = True
+    else:
+        desk = False
+
     if type == "img":
         # Load the image
-        docs = [Image.open(doc)]
+        docs = [imread(doc)]
 
     elif type == "im_dir":
         # Get the list of image files in the doc directory
         image_files = [os.path.join('doc', file) for file in os.listdir('doc') if file.endswith(('.jpg', '.jpeg', '.png'))]
 
         # Load the images
-        docs = [Image.open(file) for file in image_files]
+        docs = [imread(file) for file in image_files]
 
     elif type == "pdf":
         docs = convert_from_path(doc)
@@ -53,6 +76,11 @@ def main():
     merger = PdfMerger()
 
     for img in docs:
+
+        if desk:
+            # Deskew the images
+            img = deskew(img)
+        
         pdf = pytesseract.image_to_pdf_or_hocr(img, lang='eng', extension='pdf')
         pdf_bytes_stream = io.BytesIO(pdf)
         pdf = PdfReader(pdf_bytes_stream)
