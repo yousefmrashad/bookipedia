5deba916e7c6f6609a5a92396797beefbb53a381
mohamed-hazem <mohamedhazem346@gmail.com>
2024-03-18 03:13:19 +0200

    new test

--- python-ai-server/RAG/db_test.py
+++ python-ai-server/RAG/db_test.py
@@ -4,24 +4,26 @@ from utils.init import *
 from utils.db_config import DB
 
 # Modules
-from preprocessing.preprocess import DocumentPreprocess
+from preprocessing.document_class import Document
 from preprocessing.embeddings_class import AnglEEmbedding
 from RAG.weaviate_class import Weaviate
 # ================================================== #
 
 doc_path = r"C:\Users\LEGION\Desktop\Transformer_Network.pdf"
-doc_id = "LionelMessi181269"
-# doc_id = "LionelMessi1812"
+doc_id = "transformer-book69"
 
 client = DB().connect()
 db = Weaviate(client, embedder=AnglEEmbedding())
 
-# print("Processing Document...")
-# DocumentPreprocess(doc_path, doc_id).preprocess(client)
+print("Deleting...") 
+db.delete(doc_id)
 
-print("Performing some shit...")
-q = "What is positional encoding in transformers?"
-docs = db.similarity_search(query=q, source_ids=[doc_id], k=20)
+print("Processing...")
+document = Document(doc_path, doc_id)
+document.preprocess(client)
+
+q = "Give me the introduction of this book"
+docs = db.similarity_search(query=q, source_ids=[doc_id])
 
 print("="*50)
 for doc, score in docs:
@@ -30,3 +32,8 @@ for doc, score in docs:
     print("="*50)
 
 client.close()
+
+# f_obj = client.collections.get(DB_NAME).query.fetch_objects(limit=1, sort=SORT).objects[0]
+# print(f_obj.properties["page_no"])
+# print(f_obj.properties["text"])
+# client.close()
